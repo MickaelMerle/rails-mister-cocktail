e5fd409d6eb9100f01fce400960be6b7665cf831
mickael merle <mickaelmerle6901@gmail.com>
2020-02-01 16:09:05 -0500

    index view done

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,2 +1,34 @@
 class CocktailsController < ApplicationController
+  before_action :cocktail_find, only: [:show, :destroy]
+
+  def index
+    @cocktails = Cocktail.all
+  end
+
+  def show
+  end
+
+  def new
+    @cocktail = Cocktail.new
+  end
+
+  def create
+    @cocktail = cocktail.create(cocktail_params)
+    redirect_to cocktail_path(@new_cocktail)
+  end
+
+  def destroy
+    @cocktail = Cocktail.destroy
+    redirect_to cocktail_path(@cocktail)
+  end
+
+  private
+
+  def cocktail_params
+    params.require(:cocktail).permit(:name)
+  end
+
+  def cocktail_find
+    @cocktail = Cocktail.find(params[:id])
+  end
 end
--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,2 +1,26 @@
 class DosesController < ApplicationController
+  def new
+    @dose = Dose.find(params[:dose_id])
+    @dose = Dose.new
+  end
+
+  def create
+    @dose = Dose.new(dose_params)
+    @dose = Restaurant.find(params[:restaurant_id])
+    @review.restaurant = @restaurant
+    @review.save
+    redirect_to restaurant_path(@restaurant)
+  end
+
+  def delete
+  end
+
+  private
+
+  def dose_params
+    params.require(:review).permit(:content, :rating)
+  end
 end
+
+end
+
