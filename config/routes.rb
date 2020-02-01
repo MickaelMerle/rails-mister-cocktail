ca1dfaf275457627b2c1b843a4add94a2bbcba7f
mickael merle <mickaelmerle6901@gmail.com>
2020-02-01 15:09:33 -0500

    routes done

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :cocktails, except: [:edit, :update] do
-    resources :doses, only [:new, :create, :delete]
+    resources :doses, only: [:new, :create, :delete]
   end
 end
 
